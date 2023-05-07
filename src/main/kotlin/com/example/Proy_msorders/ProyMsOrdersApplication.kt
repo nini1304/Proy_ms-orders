dbbb43dd2507366e0cbafe3b16c6d5ada50f371f
calc <geraldine.ibieta13@gmail.com>
2023-05-06 23:19:08 -0400

    Creacion del proyecto

---
+++ src/main/kotlin/com/example/Proy_msorders/ProyMsOrdersApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Proy_msorders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyMsOrdersApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyMsOrdersApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Proy_msorders/ProyMsOrdersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Proy_msorders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyMsOrdersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
